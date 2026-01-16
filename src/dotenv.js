439ef891b96f6207ac81fcbbc132779c019c6b42
Jan Miksovsky <jan@miksovsky.com>
2026-01-16 10:34:51 -0800

    Specify location of .env file

--- src/dotenv.js
+++ src/dotenv.js
@@ -1,5 +1,10 @@
-import "dotenv/config";
-import process from "node:process";
+import dotenv from "dotenv";
+import process from "process";
+
+dotenv.config({
+  path: new URL("../.env", import.meta.url).pathname,
+  quiet: true,
+});
 
 // Offer access to environment variables after loading .env file.
 export default () => process.env;
